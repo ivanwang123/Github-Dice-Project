30a8f4f30b631f9c3c133806946021b867846476
ivanwang123 <45395180+ivanwang123@users.noreply.github.com>
2018-12-05 09:23:51 -0500

    Added Test 2
    Created Test 2 in the DiceRunner class

--- DiceRunner.java
+++ DiceRunner.java
@@ -13,12 +13,24 @@ public class DiceRunner {
 		while(rolling) {
 			rolls++;
 			diceHolder.shake();
-			diceHolder.toString();
+			System.out.println(diceHolder.toString());
 
 			if (die1.getValue() == die2.getValue() == 1) {
 				rolling = false;
 			}
 		}
 		System.out.println("It took " + rolls + " rolls to get a snake eye");
+
+		//Test 2
+		DiceHolder diceHolder2 = new DiceHolder();
+
+		int full = 1;
+		while(full != -1) {
+			int sides = (int)(Math.random() * 10) + 2
+			Die die = new Die(sides);
+			full = diceHolder2.addDie(die);
+		}
+		diceHolder2.shake();
+		System.out.println(diceHolder2.toString());
 	}
 }
