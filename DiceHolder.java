cc4281ea86cc7d72ec6d73ba2d4e9cd3f5edbfcf
adyagupta <45395088+adyagupta@users.noreply.github.com>
2018-12-07 09:30:58 -0500

    to String method
    Returns the dice  in separate lines

--- DiceHolder.java
+++ DiceHolder.java
@@ -2,27 +2,29 @@ import java.util.ArrayList;
 public class DiceHolder{
 
 	ArrayList <Die> diceholder = new ArrayList<>();
-	int Die;
+
 
 	public DiceHolder(){
 	}
 	public int addDie(Die die){
 		if(diceholder.size()==6)
 			return -1;
-		if(diceholder.size()!=6){
+		if(diceholder.size()!=6)
 			diceholder.add(die);
 		return 1;
-	}
-	return Die;
 	}
 	public void shake(){
 		for(int i = 0; i<diceholder.size(); i++){
 			diceholder.get(i).roll();
 		}
 	}
-	//public String toString(){
-		//return
-	//}
+	public String toString(){
+		String a = "";
+		for(int i = 0; i<diceholder.size(); i++)
+			a+= diceholder.get(i)+"\n";
+
+		return "Die: "+ a;
+	}
 
 
 
